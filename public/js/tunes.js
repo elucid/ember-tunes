96795a7d2588a21cd66a2540e2079bdc0cf6c314
Justin Giancola <justin.giancola@gmail.com>
2013-02-08 22:39:45 -0500

    Make /library default route; basic sidebar/panel structure with dummy templates

--- public/js/tunes.js
+++ public/js/tunes.js
@@ -1,1 +1,13 @@
 window.Tunes = Ember.Application.create();
+
+Tunes.Router.map(function() {
+  this.route('library');        // this is our default route
+});
+
+// TODO: explain ApplicationRoute and IndexRoute defaults and why we override
+// here for a different default route
+Tunes.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('library');
+  }
+});
