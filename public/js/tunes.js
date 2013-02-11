68daba808919deb9cd7ae01f3ec1d30652cdc55d
Justin Giancola <justin.giancola@gmail.com>
2013-02-11 16:51:41 -0500

    Add audio element and complete player controller actions

--- public/js/tunes.js
+++ public/js/tunes.js
@@ -58,6 +58,54 @@ Tunes.PlayerView = Em.View.extend({
   tagName: 'nav'
 });
 
+Tunes.PlayerController = Em.Controller.extend({
+  needs: ['playlist'],
+
+  currentTrack: Em.computed.alias('controllers.playlist.currentTrack'),
+
+  isPlaying: false,
+
+  init: function(){
+    this._super();
+
+    var audio = new Audio();
+
+    audio.addEventListener('ended', function() {
+      this.get('target').send('next');
+    }.bind(this));
+
+    this.set('audio', audio);
+  },
+
+  play: function() {
+    if (!this.get('currentTrack')) {
+      return;
+    }
+
+    this.get('audio').play();
+    this.set('isPlaying', true);
+  },
+
+  pause: function() {
+    this.get('audio').pause();
+    this.set('isPlaying', false);
+  },
+
+  currentTrackChanged: function() {
+    var new_src = this.get('currentTrack.url');
+
+    this.get('audio').src = new_src;
+
+    if (!new_src) {
+      this.pause();
+    }
+
+    if (this.get('isPlaying')) {
+      this.play();
+    }
+  }.observes('currentTrack')
+});
+
 Tunes.PlaylistController = Em.ArrayController.extend({
   currentTrack: null,
 
